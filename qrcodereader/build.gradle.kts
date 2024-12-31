86ddb07d876df17869db9db108f640ed6c057376
jithukrishnaju <jithukju@tigmaminds.com>
2024-12-31 11:58:20 +0530

    themes changes

--- qrcodereader/build.gradle.kts
+++ qrcodereader/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility JavaVersion.VERSION_17
+                targetCompatibility JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -60,20 +60,9 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("com.google.mlkit:barcode-scanning:17.3.0")
-//    implementation("com.google.android.gms:play-services-code-scanner:16.1.0")
-//    implementation("com.google.android.gms:play-services-base:18.5.0")
     val camerax_version = "1.5.0-alpha04"
-    // The following line is optional, as the core library is included indirectly by camera-camera2
     implementation("androidx.camera:camera-core:${camerax_version}")
     implementation("androidx.camera:camera-camera2:${camerax_version}")
-    // If you want to additionally use the CameraX Lifecycle library
     implementation("androidx.camera:camera-lifecycle:${camerax_version}")
-    // If you want to additionally use the CameraX VideoCapture library
-//    implementation("androidx.camera:camera-video:${camerax_version}")
-    // If you want to additionally use the CameraX View class
     implementation("androidx.camera:camera-view:${camerax_version}")
-    // If you want to additionally add CameraX ML Kit Vision Integration
-//    implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
-//    // If you want to additionally use the CameraX Extensions library
-//    implementation("androidx.camera:camera-extensions:${camerax_version}")
 }
